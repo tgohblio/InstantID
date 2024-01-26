cfc446307aa59bfd7ec631e051d28f1463476a6b
Terentius <tgohblio@gmail.com>
2024-01-26 13:28:27 +0000

    using huggingface library to download

--- cog/download-weights.py
+++ cog/download-weights.py
@@ -5,6 +5,7 @@ import sys
 import torch
 import time
 import subprocess
+from huggingface_hub import hf_hub_download
 from diffusers import StableDiffusionXLPipeline
 
 # append project directory to path so predict.py can be imported
@@ -14,9 +15,6 @@ from predict import SD_MODEL_NAME, SD_MODEL_CACHE
 
 # for ip-adapter and ControlNetModel
 CHECKPOINTS_CACHE = "./checkpoints"
-CHECKPOINT_IP_ADAPTER_URL = "https://huggingface.co/InstantX/InstantID/blob/main/ip-adapter.bin"
-CHECKPOINT_CTRLNET_URL = "https://huggingface.co/InstantX/InstantID/blob/main/ControlNetModel/diffusion_pytorch_model.safetensors"
-CHECKPOINT_CTRLNET_CFG_URL = "https://huggingface.co/InstantX/InstantID/blob/main/ControlNetModel/config.json"
 
 # for `models/antelopev2`
 MODELS_CACHE = "./models"
@@ -46,6 +44,6 @@ if not os.path.exists(MODELS_CACHE):
     download_weights(MODELS_URL, MODELS_CACHE)
 
 if not os.path.exists(CHECKPOINTS_CACHE):
-    download_weights(CHECKPOINT_IP_ADAPTER_URL, CHECKPOINTS_CACHE)
-    download_weights(CHECKPOINT_CTRLNET_URL, f"{CHECKPOINTS_CACHE}/ControlNetModel")
-    download_weights(CHECKPOINT_CTRLNET_CFG_URL, f"{CHECKPOINTS_CACHE}/ControlNetModel")
+    hf_hub_download(repo_id="InstantX/InstantID", filename="ip-adapter.bin", local_dir=CHECKPOINTS_CACHE)
+    hf_hub_download(repo_id="InstantX/InstantID", filename="ControlNetModel/config.json", local_dir=CHECKPOINTS_CACHE)
+    hf_hub_download(repo_id="InstantX/InstantID", filename="ControlNetModel/diffusion_pytorch_model.safetensors", local_dir=CHECKPOINTS_CACHE)
